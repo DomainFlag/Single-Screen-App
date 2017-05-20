eae61d91cc6e43aec24882a6980bf0767646e916
DomainFlag <Cchivriga@hotmail.com>
2017-05-20 14:03:46 +0200

    initial commit

---
+++ app/src/main/java/com/example/cchiv/elipili/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cchiv.elipili;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
